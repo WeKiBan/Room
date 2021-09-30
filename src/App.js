095e13d59e28d403ced09e89ad83bf833b871e5e
Wekiban <wes.banyard@googlemail.com>
2021-09-30 19:01:31 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    body: '#fff',
+    darkGrey: 'hsl(0, 0%, 63%)',
+    vDarkGray: 'hsl(0, 0%, 27%)',
+    black: 'hsl(0, 0%, 0%)',
+    white: 'hsl(0, 0%, 100%)',
+  },
+  font: {
+    bodySize: '12px',
+  },
+  mobile: '375px',
+  desktop: '1440px',
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <div className="App"></div>
+    </ThemeProvider>
   );
 }
 

d693369cfb8fbf2aca4f3fd1bd9b2098fcd82e99
Wekiban <wes.banyard@googlemail.com>
2021-10-04 16:48:03 +0200

    changed data

--- src/data.js
+++ src/data.js
@@ -12,14 +12,14 @@ export const heroAssets = [
     id: 1,
     title: 'Discover innovative ways to decorate',
     paragraph:
-      ' We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.',
+      'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.',
     images: [hero1, mobile1],
   },
   {
     id: 2,
-    title: 'Shop now We are available all across the globe',
+    title: 'Shop now we are available all across the globe',
     paragraph:
-      " With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us",
+      "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us",
     images: [hero2, mobile2],
   },
   {
